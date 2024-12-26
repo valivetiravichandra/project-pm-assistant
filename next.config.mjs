243383da2ca33d91f864401ed3c66b7162f4aa7f
Ravi Chandra Valiveti <ravichandravaliveti@mac.lan>
2024-12-26 17:35:50 -0500

    Fix: Replace Geist fonts with Inter and JetBrains Mono

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'standalone',
+    poweredByHeader: false,
+    reactStrictMode: true,
+    swcMinify: true
+  };
+  
+  export default nextConfig;
