f888530ff48861138647977346a39bfb906f89b4
Ravi Chandra Valiveti <ravichandravaliveti@mac.lan>
2024-12-26 17:25:47 -0500

    another built

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  poweredByHeader: false,
+  reactStrictMode: true,
+  swcMinify: true
 };
 
 export default nextConfig;
